2dd26a66f7541da2fb441335fabe896480511ee5
aryaramandika <93310899+aryaramandika@users.noreply.github.com>
2023-12-12 21:03:27 +0700

    Add files via upload

--- penjualan.sql
+++ penjualan.sql
@@ -1,10 +1,10 @@
-
 CREATE TABLE barang (
-  kdbrg char(5) DEFAULT NULL,
-  nama varchar(10) DEFAULT NULL,
-  satuan varchar(10) DEFAULT NULL,
-  hargasatuan int(11) DEFAULT NULL,
-  stok int(11) DEFAULT NULL
+  kdbrg CHAR(5) DEFAULT NULL,
+  nama VARCHAR(10) DEFAULT NULL,
+  satuan VARCHAR(10) DEFAULT NULL,
+  hargasatuan DECIMAL(10, 2) DEFAULT NULL,
+  stok INT DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 INSERT INTO barang VALUES ('B001','Buku Tulis','pak',45000,30),('B002','Pensil','lusin',45600,24),('B003','Map','biji',7800,12),('B004','Penghapus','biji',6000,6),('B005','Pena','biji',17200,18);
